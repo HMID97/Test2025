69ba5aba0c1cf3f3ab9d2d5e13163f248a97564d
hm0d <106934512+xHM0D@users.noreply.github.com>
2025-02-05 06:51:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ client.on('ready', async () => {
   console.log(`${client.user.tag} - rich presence started!`);
 
   const r = new Discord.RichPresence()
-    .setApplicationId('1265825059692609587')
+    .setApplicationId('939661271308046366')
     .setType('PLAYING')
     .setURL(' ') 
     .setState(' ')
