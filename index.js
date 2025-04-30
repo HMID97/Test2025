b3f805f3a6b8c70e672edfb88027e1a9fb198cac
HMID97 <99371558+HMID97@users.noreply.github.com>
2025-04-30 18:54:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ client.on('ready', async () => {
   const r = new Discord.RichPresence()
     .setApplicationId('1265825059692609587')
     .setType('PLAYING')
-    .setName('Netflix')
+    .setName('Google Chrome')
     .setStartTimestamp(Date.now())
 
   client.user.setActivity(r);
